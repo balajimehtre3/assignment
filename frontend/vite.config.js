f6fad2254982046105cab9a64ecba5083d94bcaf
Balaj Mhetre <balaji3mehtre@gmail.com>
2026-09-08 10:38:10 +0530

    config updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
+  preview: {
+    allowedHosts: ['assignment-2-7l0e.onrender.com'],
+  },
+
   server: {
     port: 5173,
+
     proxy: {
       '/api': {
-        target: 'https://backend-delta-murex-24.vercel.app',
+        target: 'https://backend-two-xi-91.vercel.app',
         changeOrigin: true,
         secure: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
